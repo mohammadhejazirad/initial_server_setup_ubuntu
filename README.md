7aee6bccd84debdd41daa71d2cd537823ee15869
MrHr <hejazi00831@gmail.com>
2023-12-05 21:58:38 +0330

    update dns menu and readme file

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ## script for initial setup ubuntu server
 
 ### menu items:
+
 * 0- exit from script
 * 1- install essential items server
-* 2- set dns for iran servers ([403](https://403.online/))
+* 2- set dns for iran
+  servers ([403](https://403.online/)) || ([shecan](https://shecan.ir/)) || ([hostIran](https://hostiran.net/landing/proxy))
 * 3- add | remove ir repository ubuntu
 * 4- enable ssh for root
 * 5- change password root
@@ -14,9 +16,18 @@ for ease of doing various tasks on Ubuntu server
 ```bash
 sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/mohammadhejazirad/initial_server_setup_ubuntu/main/setup.bash)"
 ```
+
 or
 
 ```bash
 sudo bash -c "$(wget https://raw.githubusercontent.com/mohammadhejazirad/initial_server_setup_ubuntu/main/setup.bash -O -)"
+```
+
+```bash
+chmod +x ./setup.bash
+```
+
+```bash
+sudo ./setup.bash
 ```
 
--- setup.bash
+++ setup.bash
@@ -95,6 +95,14 @@ function echo_green {
   echo -e "\e[32m$1\e[0m"
 }
 
+function check_resolvconf {
+  if command -v resolvconf &>/dev/null; then
+    echo_green "resolvconf is installed."
+  else
+    sudo apt install -y resolvconf
+  fi
+}
+
 function display_menu {
   echo "Please select an option:"
   echo_purple "0. Exit From Script"
@@ -115,10 +123,66 @@ function install_essential_items {
   sudo apt install -y nano htop neofetch resolvconf git build-essential g++ vim ranger
 }
 
-function set_dns {
-  sudo apt install -y resolvconf
+function menu_set_dns {
+  check_resolvconf
+
+  while true; do
+    echo "Please select DNS configuration:"
+    echo_green "0. back to home menu"
+    echo_blue "1. 403 online"
+    echo_purple "2. shecan"
+    echo_yellow "3. host iran"
+
+    read -p "Enter your choice (0-3): " choice
+
+    case $choice in
+    0)
+      echo
+      echo_green_bold "Returning to the main menu."
+      echo
+      return
+      ;;
+    1) set_dns_403 ;;
+    2) set_dns_shecan ;;
+    3) set_dns_host_iran ;;
+
+    esac
+  done
+}
+
+function set_dns_403 {
+  # set dns to resolv
+  dns_servers=("10.202.10.202" "10.202.10.102")
+  echo -e "nameserver ${dns_servers[0]}\nnameserver ${dns_servers[1]}" | sudo tee /etc/resolv.conf >/dev/null
+  # set dns to resolv.conf.d/head
   echo "nameserver 10.202.10.202" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
   echo "nameserver 10.202.10.102" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+  sudo resolvconf -u
+  sudo systemctl restart resolvconf.service
+  sudo systemctl restart systemd-resolved.service
+  echo_green "DNS settings updated and services restarted."
+}
+
+function set_dns_shecan {
+  # set dns to resolv
+  dns_servers=("178.22.122.100" "185.51.200.2")
+  echo -e "nameserver ${dns_servers[0]}\nnameserver ${dns_servers[1]}" | sudo tee /etc/resolv.conf >/dev/null
+  # set dns to resolv.conf.d/head
+  echo "nameserver 178.22.122.100" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+  echo "nameserver 185.51.200.2" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+  sudo resolvconf -u
+  sudo systemctl restart resolvconf.service
+  sudo systemctl restart systemd-resolved.service
+  echo_green "DNS settings updated and services restarted."
+}
+
+function set_dns_host_iran {
+  # set dns to resolv
+  dns_servers=("172.29.0.100" "172.29.2.100")
+  echo -e "nameserver ${dns_servers[0]}\nnameserver ${dns_servers[1]}" | sudo tee /etc/resolv.conf >/dev/null
+  # set dns to resolv.conf.d/head
+  echo "nameserver 172.29.0.100" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
+  echo "nameserver 172.29.2.100" | sudo tee -a /etc/resolvconf/resolv.conf.d/head
   sudo resolvconf -u
   sudo systemctl restart resolvconf.service
   sudo systemctl restart systemd-resolved.service
@@ -218,7 +282,7 @@ while true; do
     ;;
 
   1) install_essential_items ;;
-  2) set_dns ;;
+  2) menu_set_dns ;;
   3) change_ir_repository ;;
   4) enable_ssh_for_root ;;
   5) change_password_root ;;
